2438380ee81bd245d9b5b52ccc3d59f6fa249956
monaa-rm <monaa.rm95@gmail.com>
2025-12-10 02:13:09 +0330

    output standalone added to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output : 'standalone',
   images: {
     domains: [
       // "mernblogbucketimage.storage.iran.liara.space"
