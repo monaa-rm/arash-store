b531e16eaed38b3bb70ed7255d1f940565a8faae
monaa-rm <monaa.rm95@gmail.com>
2026-07-14 22:10:18 +0430

    some changes

--- src/components/footer/footer-address/index.jsx
+++ src/components/footer/footer-address/index.jsx
@@ -2,7 +2,7 @@
 const FooterAddress = ({ data }) => {
   return (
     <div className="w-full sm:w-1/2 md:w-1/4  p-4 flex flex-col gap-4">
-      <h1 className="border-b border-zinc-700 font-bold pb-2">آدرس فروشگاه</h1>
+      <h1 className="border-b border-zinc-700 font-bold pb-2">آدرس</h1>
       <ul>
         {data?.address?.length &&
           data?.address?.map((address, i) => (
--- src/components/search/search-page/index.jsx
+++ src/components/search/search-page/index.jsx
@@ -2,11 +2,15 @@
 import SearchCategoryFilter from "@/components/elements/search-category-filter";
 import SearchPriceFilter from "@/components/elements/search-price-filter";
 import SearchMainBox from "../search-main-box";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { setHighestPrice, setSearchPrice } from "@/features/filterSlice";
+import { useSearchParams } from "next/navigation";
 
 const SearchPage = ({ highestPrice }) => {
+    const searchParams = useSearchParams();
+  const [currentPage, setCurrentPage] = useState(searchParams.get("page") || 1);
+
   const dispatch = useDispatch();
   useEffect(() => {
     if (highestPrice && highestPrice > 0) {
@@ -19,7 +23,10 @@ const SearchPage = ({ highestPrice }) => {
     <div className="w-full px-2 sm:px-4 lg:px-8 py-8 flex gap-4 relative">
       <div className="w-1/3 min-w-1/3 h-full hidden lg:flex  sticky top-[100px] flex-col gap-6  ">
         {highestPrice && highestPrice > 0 ? <SearchPriceFilter /> : <></>}
-        <SearchCategoryFilter />
+        <SearchCategoryFilter
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
       </div>
       <SearchMainBox />
     </div>
